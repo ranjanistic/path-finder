3e60aae889d753bbdbfdcea44ce0202a10e5b244
ranjanistic <priyanshuranjan88@gmail.com>
2019-08-10 15:12:27 +0530

    Project Completed.
    Enjoy the flash!

--- Saber/app/src/main/java/com/pathfinder/saber/MainActivity.java
+++ Saber/app/src/main/java/com/pathfinder/saber/MainActivity.java
@@ -1,152 +1,85 @@
 package com.pathfinder.saber;
 
-import android.Manifest;
-import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
-import android.os.Build;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.Window;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
-import java.security.Policy;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends Activity {
-    private boolean flash, flashOn;
-    private ImageButton btn;
-    private Camera mcamera;
-    private android.hardware.Camera.Parameters fparams;
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+
+    private ToggleButton toggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 9999);
-        }
-        btn = findViewById(R.id.onOff);
 
-        flash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-        if (!flash) {
-            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
-            alert.setTitle("Hardware Missing");
-            alert.setMessage("No suitable hardware detected on your device. Can't start the application.");
-            alert.setButton(1, "Close", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            });
-            alert.show();
+        boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+            showNoFlashError();
         }
 
-        getCamWare();
 
-        toggleImage();
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        toggleButton = findViewById(R.id.toggleButton);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                if (flashOn) {
-                    turnOff();
-                } else {
-                    turnOn();
-                }
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                switchFlashLight(isChecked);
+                toggleImag(isChecked);
             }
         });
     }
 
-    private void getCamWare(){
-        if(mcamera == null){
-            try{
-                mcamera = new Camera();
-                //fparams = mcamera.get;
-            } catch (RuntimeException e){
-                Log.e("Camera Error.", e.getMessage());
+    public void showNoFlashError() {
+        AlertDialog alert = new AlertDialog.Builder(this)
+                .create();
+        alert.setTitle("Oops!");
+        alert.setMessage("Flash not available in this device...");
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
             }
+        });
+        alert.show();
+    }
+
+    public void switchFlashLight(boolean status) {
+        try {
+            mCameraManager.setTorchMode(mCameraId, status);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 
-    private void turnOn(){
-        if(!flashOn){
-            if(mcamera == null || fparams == null){
-                return;
-            }
-
-            //fparams = mcamera.getParameters();
-            fparams.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
-            //mcamera.setParameters(fparams);
-            mcamera.notify();
-            flashOn = true;
-
-            toggleImage();
-        }
-    }
-
-    public void turnOff(){
-        if(flashOn){
-            if(mcamera == null || fparams == null){
-                return;
-            }
-
-            //fparams = mcamera.getParameters();
-            fparams.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_OFF);
-            //mcamera.setParams(fparams);
-           // mcamera.stop();
-            flashOn = false;
-
-            toggleImage();
-        }
-    }
-
-    private void toggleImage(){
-        if(flashOn){
-            btn.setImageResource(R.drawable.ic_launcher_background);
+    public void toggleImag(boolean status){
+        if(status){
+            toggleButton.setButtonDrawable(R.drawable.ic_lamplit);
         } else{
-            btn.setImageResource(R.drawable.ic_launcher_foreground);
+            toggleButton.setButtonDrawable(R.drawable.ic_lampunlit);
         }
     }
 
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        turnOff();
-    }
-
-    @Override
-    protected void onRestart(){
-        super.onRestart();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        if(flash)
-            turnOn();
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        getCamWare();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-
-        if(mcamera!=null){
-           // mcamera.release();
-            mcamera = null;
-        }
-    }
 }
--- Torch/app/src/main/native-lib.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Priyanshu on 28-Jun-19.
-//
-
-using namespace std;
-void main(){
- printf("Hello android, I'm c plus plus");
-}
