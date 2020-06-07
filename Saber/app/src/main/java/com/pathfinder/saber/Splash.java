1cff50fb60f5062deb0d6b79fbf95391a1038d9d
ranjanistic <priyanshuranjan88@gmail.com>
2020-06-08 03:40:10 +0530

    several changes
    + add landscape layout
    + change font
    + change buttons, images
    + modify animations

--- Saber/app/src/main/java/com/pathfinder/saber/Splash.java
+++ Saber/app/src/main/java/com/pathfinder/saber/Splash.java
@@ -2,8 +2,8 @@ package com.pathfinder.saber;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
